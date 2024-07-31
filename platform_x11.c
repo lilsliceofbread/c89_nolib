66d8b074853c13d8961c5c4e3235e59b8386f99d
lilsliceofbread <yeshumankar@gmail.com>
2024-07-31 22:38:09 +0800

    everything WORKS but image too small

--- platform_x11.c
+++ platform_x11.c
@@ -37,20 +37,20 @@ void ls_x11_run(void)
 {
     /* must declare variables at start of function in c89 */
     NB_INT x, y;
-    XExposeEvent expose_event;
     XWindowAttributes attributes;
+    NB_INT width, height;
 
     platform.closing = NB_FALSE;
     platform.framecounter = 0;
-    memset(&expose_event, 0, sizeof(XExposeEvent));
 
     /* open window */
     platform.display = XOpenDisplay(NULL); /* !!! should check for errors on all these */
-    platform.window = XCreateSimpleWindow(platform.display, XDefaultRootWindow(platform.display), 0, 0,
-                                          NB_WIDTH * 4, NB_HEIGHT * 4, 0, 0, 0x00000000);
+    platform.window = XCreateSimpleWindow(platform.display, XDefaultRootWindow(platform.display), 0, 0, NB_WIDTH * 4, NB_HEIGHT * 4, 0, 0, 0x00000000);
 
     XStoreName(platform.display, platform.window, NB_TITLE);
     XMapWindow(platform.display, platform.window);
+
+    XSelectInput(platform.display, platform.window, KeyPressMask | KeyReleaseMask);
 
     /* get info to handle window close event since x11 doesn't have that natively... */
     platform.wm_protocols = XInternAtom(platform.display, "WM_PROTOCOLS", NB_FALSE);
@@ -60,7 +60,7 @@ void ls_x11_run(void)
     /* create image */
     XGetWindowAttributes(platform.display, platform.window, &attributes);
     /* !!! check if depth = 1 is correct */
-    platform.image = XCreateImage(platform.display, attributes.visual, 1, ZPixmap, 0, (char*)platform.buffer,
+    platform.image = XCreateImage(platform.display, attributes.visual, attributes.depth, ZPixmap, 0, (char*)platform.buffer,
                                   NB_WIDTH, NB_HEIGHT, 32, NB_WIDTH * sizeof(NB_RGB));
 
     /* run the game */
@@ -74,21 +74,30 @@ void ls_x11_run(void)
             for (y = 0; y < NB_HEIGHT; y ++)
                 platform.buffer[x + y * NB_WIDTH] = nb_game.palette[nb_game.screen[x + y * NB_WIDTH]];
 
-        /* put image on screen? or */
-        /* send Expose event */
-        expose_event.display = platform.display; /* MORE? */
+        XGetWindowAttributes(platform.display, platform.window, &attributes);
         
-        XSendEvent(platform.display, platform.window, NB_FALSE, ExposureMask, (XEvent*)&expose_event);
 
-        /* TESTING */
-        XPutImage(platform.display, platform.window, XDefaultGC(platform.display, 0), platform.image, 0, 0, 0, 0, NB_WIDTH, NB_HEIGHT); 
+        if(attributes.width < attributes.height)
+        {
+            width = (attributes.width / NB_WIDTH) * NB_WIDTH;
+            height = width / (float)(NB_WIDTH)/(float)(NB_HEIGHT);
+        }
+        else
+        {
+            height = (attributes.height / NB_HEIGHT) * NB_HEIGHT;
+            width = height * (float)(NB_WIDTH)/(float)(NB_HEIGHT);
+        }
+
+        /* scale image */
+
+        XPutImage(platform.display, platform.window, XDefaultGC(platform.display, 0), platform.image, 0, 0, 0, 0, NB_WIDTH, NB_HEIGHT);
 
         /* poll events */
         ls_x11_poll();
 
         /* faking v-sync */
         platform.framecounter += (1000.0 / NB_FRAMERATE);
-        sleep((NB_INT)(platform.framecounter / 1000.0));
+        usleep((NB_INT)(platform.framecounter * 1000.0));
         platform.framecounter -= (NB_INT)platform.framecounter;
     }
     
@@ -103,12 +112,18 @@ void ls_x11_poll(void)
     KeySym key;
 
     /* loop over events until there are none left */
-    while (XCheckWindowEvent(platform.display, platform.window, ExposureMask | KeyPressMask | KeyReleaseMask, &event))
+    while (XPending(platform.display))
     {
+        XNextEvent(platform.display, &event);
         switch(event.type)
         {
+            case ClientMessage:
+                if(event.xclient.message_type == platform.wm_protocols
+                && event.xclient.data.l[0] == platform.wm_delete_window)
+                    platform.closing = NB_TRUE;
+                break;
             case KeyPress:
-                key = XKeysymToKeycode(platform.display, event.xkey.keycode);
+                key = XLookupKeysym(&event.xkey, 0);
                 
                 if (key == XK_d || key == XK_Right) nb_game.btn[NB_RIGHT] = NB_TRUE;
                 if (key == XK_a || key == XK_Left) nb_game.btn[NB_LEFT] = NB_TRUE;
@@ -117,28 +132,15 @@ void ls_x11_poll(void)
                 break;
 
             case KeyRelease:
-                key = XKeysymToKeycode(platform.display, event.xkey.keycode);
+                key = XLookupKeysym(&event.xkey, 0);
                 
                 if (key == XK_d || key == XK_Right) nb_game.btn[NB_RIGHT] = NB_FALSE;
                 if (key == XK_a || key == XK_Left) nb_game.btn[NB_LEFT] = NB_FALSE;
                 if (key == XK_w || key == XK_Up) nb_game.btn[NB_UP] = NB_FALSE;
                 if (key == XK_s || key == XK_Down) nb_game.btn[NB_DOWN] = NB_FALSE;
                 break;
-
-            case Expose: /* equivalent to WM_PAINT */
-                XPutImage(platform.display, platform.window, XDefaultGC(platform.display, 0), platform.image, 0, 0, 0, 0, NB_WIDTH, NB_HEIGHT); 
-                XDrawString(platform.display, platform.window, XDefaultGC(platform.display, 0), 0, 0, "THINGLY", 7);
-                break;
         }
     }
-
-    /* XCheckWindowEvent ignores ClientMessage events so do this after */
-    if(!XCheckTypedWindowEvent(platform.display, platform.window, ClientMessage, &event))
-        return;
-
-    if(event.xclient.message_type == platform.wm_protocols
-       && event.xclient.data.l[0] == platform.wm_delete_window)
-       platform.closing = NB_TRUE;
 }
 
 /* without libc wrapping it, _start() is the correct entry point */
